e3c4cd77d04f186066fc573601cc9d1d31d95c8e
Valeria <valeria.lukashevich@gmail.com>
2022-11-14 16:26:18 +0200

    commit

---
+++ src/main/java/ee/valiit/back_bank_26/CityInfo.java
@@ -0,0 +1,23 @@
+package ee.valiit.back_bank_26;
+
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+public class CityInfo {
+    private String cityName;
+    private Integer cityNameId;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+    public Integer getCityNameId() {
+        return cityNameId;
+    }
+
+    public void setCityNameId(Integer cityNameId) {
+        this.cityNameId = cityNameId;
+    }
+}
