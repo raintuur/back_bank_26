fd9b72666188872fca45386b3579bca4af5f49af
Margit Toots <m.toots@hotmail.com>
2022-11-14 14:59:32 +0200

    MARGIT added DB

---
+++ db/exercises.sql
@@ -0,0 +1,40 @@
+-- todo:
+--  C - Create
+--  R - Read
+--  U - Update
+--  D - Delete
+
+
+-- todo:
+--  C - INSERT
+--  R - SELECT
+--  U - UPDATE
+--  D - DELETE
+
+SELECT atm.serial_number, l.name, atm.status AS "seisund"
+FROM atm
+         JOIN location l ON l.id = atm.location_id
+         JOIN city c ON c.id = l.city_id
+         JOIN atm_service_relation asr ON atm.id = asr.atm_id
+         JOIN atm_service "as" ON "as".id = asr.atm_service_id
+WHERE c.name = 'Tallinn'
+  AND "as".name = 'maksed';
+
+INSERT INTO "user" (username, password, role_id)
+VALUES ('rain', '123', 2);
+
+INSERT INTO customer (first_name, last_name, personal_code, user_id)
+VALUES ('Rain', 'Tüür', 'EE0001', 2);
+
+UPDATE  customer
+SET personal_code = 'EE00001'
+WHERE personal_code = 'EE0001';
+
+
+DELETE
+FROM customer
+WHERE personal_code = 'EE00001';
+
+
+
+
