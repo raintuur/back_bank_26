66906857d6b26db73450ccfb4f005c2bc99b6fc7
temp <astrid.oras@gmail.com>
2022-11-15 11:57:54 +0200

    database

---
+++ src/main/java/ee/valiit/back_bank_26/City.java
@@ -0,0 +1,14 @@
+package ee.valiit.back_bank_26;
+
+public class City {
+    private Integer cityNameId;
+    private String cityName;
+
+    public City() {
+    }
+
+    public City(Integer cityNameId, String cityName) {
+        this.cityNameId = cityNameId;
+        this.cityName = cityName;
+    }
+}
