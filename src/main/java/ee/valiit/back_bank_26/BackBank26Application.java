8086faa4be4c99e70bf56ae2f6f43d1fd16ef50f
GoodVib3s <vladislav.jagur@gmail.com>
2022-11-15 14:03:01 +0200

    Commit

--- src/main/java/ee/valiit/back_bank_26/BackBank26Application.java
+++ src/main/java/ee/valiit/back_bank_26/BackBank26Application.java
@@ -10,47 +10,47 @@ import java.util.List;
 public class BackBank26Application {
 
 
-	public static List<City> cities = new ArrayList<>();
-	public static List<AtmService> atmServices = new ArrayList<>();
+    public static List<City> cities = new ArrayList<>();
+    public static List<AtmService> atmServices = new ArrayList<>();
 
 
-	public static List<City> findAllCities() {
-		return cities;
-	}
-	public static void main(String[] args) {
-		createCityRepository();
-		createAtmCervices();
-		SpringApplication.run(BackBank26Application.class, args);
-	}
+    public static List<City> findAllCities() {
+        return cities;
+    }
 
-	private static void createAtmCervices() {
-		AtmService atmService1 = createAtmService(1, "Raha sisse");
-		AtmService atmService2 = createAtmService(2, "Raha välja");
-		AtmService atmService3 = createAtmService(3, "Maksed");
-		atmServices.add(atmService1);
-		atmServices.add(atmService2);
-		atmServices.add(atmService3);
+    public static void main(String[] args) {
+        createCityRepository();
+        createAtmCervices();
+        SpringApplication.run(BackBank26Application.class, args);
+    }
 
-	}
+    private static void createAtmCervices() {
+        AtmService atmService1 = createAtmService(1, "Raha sisse");
+        AtmService atmService2 = createAtmService(2, "Raha välja");
+        AtmService atmService3 = createAtmService(3, "Maksed");
+        atmServices.add(atmService1);
+        atmServices.add(atmService2);
+        atmServices.add(atmService3);
 
-		private static void createAtmService(Integer id, String name) {
-		AtmService atmService = new AtmService();
-		atmService.setId(id);
-		atmService.setName(name);
-		return atmService;
-	}
+    }
+
+    private static void createAtmService(Integer id, String name) {
+        AtmService atmService = new AtmService();
+        atmService.setId(id);
+        atmService.setName(name);
+        return atmService;
+    }
 
 
+    private static void createCityRepository() {
+        City city1 = new City("Tallinn", 1);
+        City city2 = new City("Tartu", 2);
+        City city3 = new City("Pärnu", 3);
+        cities.add(city1);
+        cities.add(city2);
+        cities.add(city3);
 
-	private static void createCityRepository() {
-		City city1 = new City("Tallinn", 1);
-		City city2 = new City("Tartu", 2);
-		City city3 = new City("Pärnu", 3);
-		cities.add(city1);
-		cities.add(city2);
-		cities.add(city3);
-
-	}
+    }
 
 
 }
