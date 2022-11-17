e787d2f66e87641775cb24f69f0383ac717e77da
Rain Tüür <rain.tuur@gmail.com>
2022-11-17 13:55:37 +0200

    RAIN WIP

--- src/main/java/ee/valiit/back_bank_26/BackBank26Application.java
+++ src/main/java/ee/valiit/back_bank_26/BackBank26Application.java
@@ -1,8 +1,5 @@
 package ee.valiit.back_bank_26;
 
-import ee.valiit.back_bank_26.atm.AtmServiceRepositoryImpl;
-import ee.valiit.back_bank_26.city.CityRepositoryImpl;
-import ee.valiit.back_bank_26.location.LocationRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,16 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackBank26Application {
     public static void main(String[] args) {
-        CityRepositoryImpl cityRepository = new CityRepositoryImpl();
-        cityRepository.createCityRepository();
-
-        AtmServiceRepositoryImpl atmServiceRepository = new AtmServiceRepositoryImpl();
-        atmServiceRepository.createAtmServices();
-
-        LocationRepositoryImpl locationRepository = new LocationRepositoryImpl();
-        locationRepository.createLocationRepository();
-
-
         SpringApplication.run(BackBank26Application.class, args);
     }
 
