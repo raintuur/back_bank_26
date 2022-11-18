84147d40442ec9bfa8d77d345f41881a63605d7b
Kristiina <kristiina.saarniit@yahoo.com>
2022-11-18 11:54:33 +0200

    commit

--- src/main/java/ee/valiit/back_bank_26/BackBank26Application.java
+++ src/main/java/ee/valiit/back_bank_26/BackBank26Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackBank26Application {
     public static void main(String[] args) {
+
         SpringApplication.run(BackBank26Application.class, args);
     }
 
