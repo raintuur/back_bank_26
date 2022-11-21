737e53fd48501037f80ba7c8c787a4c5f7163e31
Rain Tüür <rain.tuur@gmail.com>
2022-11-21 11:00:58 +0200

    RAIN WIP

--- src/main/java/ee/valiit/back_bank_26/domain/atm/option/OptionMapper.java
+++ src/main/java/ee/valiit/back_bank_26/domain/atm/option/OptionMapper.java
@@ -1,17 +1,24 @@
 package ee.valiit.back_bank_26.domain.atm.option;
 
+import ee.valiit.back_bank_26.atm.AtmOptionDto;
+import ee.valiit.back_bank_26.domain.atm.atmoption.AtmOption;
 import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface OptionMapper {
-    @Mapping(source = "id", target ="optionId")
-    @Mapping(source = "name", target ="optionName")
+    @Mapping(source = "id", target = "optionId")
+    @Mapping(source = "name", target = "optionName")
     @Mapping(constant = "false", target = "isSelected")
     OptionDto toDto(Option option);
 
     List<OptionDto> toDtos(List<Option> options);
+
+    @Mapping(source = "name", target = "optionName")
+    AtmOptionDto toAtmOptionDto(Option option);
+
+    List<AtmOptionDto> toAtmOptionDtos(List<Option> options);
 
 
 }
