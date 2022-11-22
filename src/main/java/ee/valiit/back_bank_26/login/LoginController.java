f0cce44b584f6449925e1c63b7a17e0b259e4836
Rain Tüür <rain.tuur@gmail.com>
2022-11-22 10:48:46 +0200

    RAIN WIP

--- src/main/java/ee/valiit/back_bank_26/login/LoginController.java
+++ src/main/java/ee/valiit/back_bank_26/login/LoginController.java
@@ -1,5 +1,6 @@
 package ee.valiit.back_bank_26.login;
 
+import ee.valiit.back_bank_26.domain.userrole.user.LoginResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,9 @@ public class LoginController {
     private LoginService loginService;
 
     @GetMapping("/login")
-    public void login(@RequestParam String username, @RequestParam String password) {
-        loginService.login(username, password);
+    public LoginResponse login(@RequestParam String username, @RequestParam String password) {
+        LoginResponse loginResponse = loginService.login(username, password);
+        return loginResponse;
     }
 
 
