c756f71f8ca623d1867a0457352fbc5ba06d0894
Kristiina <kristiina.saarniit@yahoo.com>
2022-11-22 10:02:13 +0200

    commit

---
+++ src/main/java/ee/valiit/back_bank_26/domain/userrole/role/Role.java
@@ -0,0 +1,36 @@
+package ee.valiit.back_bank_26.domain.userrole.role;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/ee/valiit/back_bank_26/domain/userrole/user/User.java
@@ -0,0 +1,61 @@
+package ee.valiit.back_bank_26.domain.userrole.user;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "\"user\"")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+
+    @NotNull
+    @Column(name = "role_id", nullable = false)
+    private Integer roleId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+}
