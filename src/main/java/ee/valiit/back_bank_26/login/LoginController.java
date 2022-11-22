bcc1edc1e564c501f0fa5b744ca3047c0835868e
Rain Tüür <rain.tuur@gmail.com>
2022-11-22 10:01:44 +0200

    RAIN WIP

---
+++ src/main/java/ee/valiit/back_bank_26/login/LoginController.java
@@ -0,0 +1,21 @@
+package ee.valiit.back_bank_26.login;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class LoginController {
+
+    @Resource
+    private LoginService loginService;
+
+    @GetMapping("/login")
+    public void login(@RequestParam String username, @RequestParam String password) {
+        loginService.login(username, password);
+    }
+
+
+}
---
+++ src/main/java/ee/valiit/back_bank_26/login/LoginService.java
@@ -0,0 +1,11 @@
+package ee.valiit.back_bank_26.login;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    public void login(String username, String password) {
+
+    }
+}
