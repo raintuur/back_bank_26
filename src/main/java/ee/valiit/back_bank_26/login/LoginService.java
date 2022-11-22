4272f4c3081198181bbb97c7e7f838fad706bcff
Rain Tüür <rain.tuur@gmail.com>
2022-11-22 10:34:19 +0200

    RAIN WIP

--- src/main/java/ee/valiit/back_bank_26/login/LoginService.java
+++ src/main/java/ee/valiit/back_bank_26/login/LoginService.java
@@ -1,11 +1,27 @@
 package ee.valiit.back_bank_26.login;
 
+import ee.valiit.back_bank_26.domain.userrole.user.User;
+import ee.valiit.back_bank_26.domain.userrole.user.UserRepository;
+import ee.valiit.back_bank_26.infrastructure.exception.BusinessException;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Optional;
 
 @Service
 public class LoginService {
 
+    @Resource
+    private UserRepository userRepository;
+
     public void login(String username, String password) {
+        Optional<User> userOptional = userRepository.findBy(username, password);
+
+        // kui ei leia siis viska viga
+        if (userOptional.isEmpty()) {
+            throw new BusinessException("Oled loll", "666");
+        }
+
 
     }
 }
