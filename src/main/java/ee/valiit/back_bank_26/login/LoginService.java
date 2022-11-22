be08f01804a477c91e1b590f3a89eb2b4571c060
Mario Mumm <mario.mumm@hotmail.com>
2022-11-22 10:45:18 +0200

    MARIO-2

--- src/main/java/ee/valiit/back_bank_26/login/LoginService.java
+++ src/main/java/ee/valiit/back_bank_26/login/LoginService.java
@@ -1,6 +1,8 @@
 package ee.valiit.back_bank_26.login;
 
+import ee.valiit.back_bank_26.domain.userrole.user.LoginResponse;
 import ee.valiit.back_bank_26.domain.userrole.user.User;
+import ee.valiit.back_bank_26.domain.userrole.user.UserMapper;
 import ee.valiit.back_bank_26.domain.userrole.user.UserRepository;
 import ee.valiit.back_bank_26.infrastructure.exception.BusinessException;
 import org.springframework.stereotype.Service;
@@ -11,16 +13,23 @@ import java.util.Optional;
 @Service
 public class LoginService {
 
+
+    @Resource
+    private UserMapper userMapper;
     @Resource
     private UserRepository userRepository;
 
-    public void login(String username, String password) {
+    public LoginResponse login(String username, String password) {
         Optional<User> userOptional = userRepository.findBy(username, password);
 
         // kui ei leia siis viska viga
         if (userOptional.isEmpty()) {
             throw new BusinessException("Oled loll", "666");
         }
+
+        User user = userOptional.get();
+        LoginResponse loginResponse = userMapper.toLoginResponse(user);
+        return loginResponse;
 
 
     }
