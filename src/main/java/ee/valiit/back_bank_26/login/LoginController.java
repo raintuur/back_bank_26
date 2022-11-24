959294880e695f26bc2b3af7f9799b77de582e8f
Mario Mumm <mario.mumm@hotmail.com>
2022-11-24 11:37:44 +0200

    MARIO-2

--- src/main/java/ee/valiit/back_bank_26/login/LoginController.java
+++ src/main/java/ee/valiit/back_bank_26/login/LoginController.java
@@ -1,13 +1,21 @@
 package ee.valiit.back_bank_26.login;
 
+import ee.valiit.back_bank_26.infrastructure.error.ApiError;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoginController {
 
     @Resource
@@ -15,6 +23,10 @@ public class LoginController {
 
     @GetMapping("/login")
     @Operation(summary = "Logib teenusesse sisse")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tagastab login objekti, kus sees on olemas kasutaja 'userId' ja tema 'roleType'"),
+            @ApiResponse(responseCode = "403", description = "Valed kasutaja rekvisiidid. Ebaõnnestunud sisselogimine", content = @Content(schema = @Schema(implementation = ApiError.class)))
+    })
     public LoginResponse login(@RequestParam String username, @RequestParam String password) {
         LoginResponse loginResponse = loginService.login(username, password);
         return loginResponse;
