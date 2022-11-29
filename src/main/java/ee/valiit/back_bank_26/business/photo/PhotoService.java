25c5dfc80700dfcccfaf0f3710ddf26bfd0f4671
Rain Tüür <rain.tuur@gmail.com>
2022-11-29 12:00:23 +0200

    RAIN WIP

--- src/main/java/ee/valiit/back_bank_26/business/photo/PhotoService.java
+++ src/main/java/ee/valiit/back_bank_26/business/photo/PhotoService.java
@@ -1,9 +1,11 @@
 package ee.valiit.back_bank_26.business.photo;
 
+import ee.valiit.back_bank_26.domain.userrole.user.User;
 import ee.valiit.back_bank_26.domain.userrole.user.UserService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class PhotoService {
@@ -13,6 +15,14 @@ public class PhotoService {
 
 
     public void addUserPhoto(PhotoRequest request) {
-        userService.getUserBy(request.getUserId());
+        User user = userService.getUserBy(request.getUserId());
+        addUserPhoto(user, request.getPictureData());
+
+
+    }
+
+    private static void addUserPhoto(User user, String pictureData) {
+        byte[] pictureAsByteArray = pictureData.getBytes(StandardCharsets.UTF_8);
+        user.setPictureData(pictureAsByteArray);
     }
 }
