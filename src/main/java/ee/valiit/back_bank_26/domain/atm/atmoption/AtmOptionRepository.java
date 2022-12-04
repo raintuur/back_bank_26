c58e477cbcc3dce35dd9b02dacceaa1a4cfb4b91
Rain Tüür <rain.tuur@gmail.com>
2022-12-04 15:59:04 +0200

    FINAL WIP

--- src/main/java/ee/valiit/back_bank_26/domain/atm/atmoption/AtmOptionRepository.java
+++ src/main/java/ee/valiit/back_bank_26/domain/atm/atmoption/AtmOptionRepository.java
@@ -7,12 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface AtmOptionRepository extends JpaRepository<AtmOption, Integer> {
-    @Query("select a from AtmOption a where a.atm.location.id = ?1 order by a.atm.location.id, a.option.id")
-    List<AtmOption> findAtmOptionsBy(Integer locationId);
 
     @Query("select distinct (a.option) from AtmOption a where a.atm.location.id = ?1")
     List<Option> findOptionsBy(Integer locationId);
-
-
 
 }
